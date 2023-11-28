2233222aca9e8ae4d262a0a5c0c1f4f0f1c2725a
Svetlana Nikulina <svetlana.nikulina@devexpress.com>
2023-11-28 11:28:27 +0400

    Update Default.aspx.cs

--- CS/Default.aspx.cs
+++ CS/Default.aspx.cs
@@ -6,20 +6,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _Default : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
-
-    protected void ASPxScheduler1_FetchAppointments(object sender, DevExpress.XtraScheduler.FetchAppointmentsEventArgs e)
-    {
+public partial class _Default : System.Web.UI.Page {
+    
+    protected void ASPxScheduler1_FetchAppointments(object sender, DevExpress.XtraScheduler.FetchAppointmentsEventArgs e) {
         SetAppointmentDataSourceSelectCommandParameters(e.Interval);
         e.ForceReloadAppointments = true;
     }
-    protected void SetAppointmentDataSourceSelectCommandParameters(TimeInterval interval)
-    {
+    protected void SetAppointmentDataSourceSelectCommandParameters(TimeInterval interval) {
         SqlDataSource1.SelectParameters["OriginalOccurrenceStart"].DefaultValue = interval.Start.ToString();
         SqlDataSource1.SelectParameters["OriginalOccurrenceEnd"].DefaultValue = interval.End.ToString();
     }
