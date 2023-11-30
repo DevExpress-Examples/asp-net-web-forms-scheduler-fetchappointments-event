0b6533097dc06a0b14b21eb5d54229de6177299d
DevExpressExampleBot <fakeEmail>
2023-11-30 09:28:58 +0400

    Source auto update [skip ci]

---
+++ VB/Default.aspx.vb
@@ -0,0 +1,20 @@
+﻿Imports DevExpress.XtraScheduler
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Protected Sub ASPxScheduler1_FetchAppointments(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.FetchAppointmentsEventArgs)
+		SetAppointmentDataSourceSelectCommandParameters(e.Interval)
+		e.ForceReloadAppointments = True
+	End Sub
+	Protected Sub SetAppointmentDataSourceSelectCommandParameters(ByVal interval As TimeInterval)
+		SqlDataSource1.SelectParameters("OriginalOccurrenceStart").DefaultValue = interval.Start.ToString()
+		SqlDataSource1.SelectParameters("OriginalOccurrenceEnd").DefaultValue = interval.End.ToString()
+	End Sub
+End Class
